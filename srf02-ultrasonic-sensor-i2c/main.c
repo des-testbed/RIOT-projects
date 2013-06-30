ab21b58dd7b328427ca16f254e2286e35ce810eb
Zakaria Kasmi <ztanger@hotmail.de>
2013-07-01 00:33:04 +0200

    application for the SRF02 ultrasonic sensor

--- srf02-ultrasonic-sensor-i2c/main.c
+++ srf02-ultrasonic-sensor-i2c/main.c
@@ -21,7 +21,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "lpc23xx.h" 	/* LPC23xx/24xx definitions */
+#include "lpc23xx.h" 	/* LPC23xx definitions */
 #include "i2c.h"
 #include "gpioint.h"
 #include "hwtimer.h"
