d266219ced4feab95e6dceaf4a406dc69ab38758
Zakaria Kasmi <zkasmi@inf.fu-berlin.de>
2013-08-23 16:56:45 +0200

    Coding conventions + Converting tabs to spaces.

--- lm75A-temp-sensor-i2c/main.c
+++ lm75A-temp-sensor-i2c/main.c
@@ -2,74 +2,77 @@
  * main.c - Main function of the LM75 temperature sensor project.
  * Copyright (C) 2013 Zakaria Kasmi <zkasmi@inf.fu-berlin.de>
  *
- * The source code is licensed under the LGPLv2 license, 
+ * The source code is licensed under the LGPLv2 license,
  * See the file LICENSE for more details.
  */
 
 /**
  * @file
  * @internal
- * @brief	Initializes the i2c interface and starts the LM75A temperature sampling function
+ * @brief       Initializes the i2c interface and starts the LM75A temperature
+ *              sampling function
  *
- * @author      Freie Universit√§t Berlin, Computer Systems & Telematics
- * @author	Zakaria Kasmi <zkasmi@inf.fu-berlin.de>
+ * @author      Freie Universit‰t Berlin, Computer Systems & Telematics
+ * @author      Zakaria Kasmi <zkasmi@inf.fu-berlin.de>
  * @version     $Revision: 3854 $
  *
- * @note	$Id: main.c 3854 2013-05-27 15:40:01 zkasmi $
+ * @note        $Id: main.c 3854 2013-09-03 18:40:03 kasmi $
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "lpc23xx.h"	/* LPC23xx/24xx definitions */
+#include "lpc23xx.h"    /* LPC23xx/24xx definitions */
 #include "i2c.h"
 #include "gpioint.h"
 #include "hwtimer.h"
 #include "board.h"
 #include "lm75a-temp-sensor.h"
 
-static void temperature_ext_interr_handler(void) {
-	puts("The external interrupt handler entered....\n");
-	lm75A_set_in_alarm(true);
-	LED_GREEN_OFF;
-	puts("....The external interrupt handler ended\n");
+static void temperature_ext_interr_handler(void)
+{
+    puts("The external interrupt handler entered....\n");
+    lm75A_set_in_alarm(true);
+    LED_GREEN_OFF;
+    puts("....The external interrupt handler ended\n");
 }
 
-static void temperature_ext_interrupt_task(void) {
-	int i;
+static void temperature_ext_interrupt_task(void)
+{
+    int i;
 
-	for (i = 0; i < 30; i++) {
-		puts(
-				"ALARMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM !!\n");
-
-	}
-	LED_GREEN_OFF;
+    for (i = 0; i < 30; i++) {
+        puts(
+            "ALARMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM !!\n");
+    }
+    LED_GREEN_OFF;
 }
 
-/*******************************************************************************
- **   Main Function  main()
- *******************************************************************************/
-int main(void) {
 
-	// init ktimer
-	hwtimer_init();
-	printf("# %-70s%10s\n", "hwtimer", "...[OK]");
+int main(void)
+{
+    // init ktimer
+    hwtimer_init();
+    printf("# %-70s%10s\n", "hwtimer", "...[OK]");
 
-	// enable interrupt handling on IO pin
-	gpioint_init();
+    // enable interrupt handling on IO pin
+    gpioint_init();
 
-	//switch on the green LED
-	LED_GREEN_ON;
+    //switch on the green LED
+    LED_GREEN_ON;
 
-	// init the I2C
-	if (lm75A_init(LM75A_I2C_INTERFACE, 100000, temperature_ext_interr_handler)) {
-		printf("# %-70s%10s\n", "lm75a initialization", "...[OK]");
-	} else {
-		printf("# %-70s%10s\n", "lm75a initialization", "...[FAILED]");
-	}
-	puts("LPC2387 is ready\n");
-	printf("PI=%f\n", 3.134343);
-	lm75A_start_sensor_sampling(temperature_ext_interrupt_task);
+    // init the I2C
+    if (lm75A_init(LM75A_I2C_INTERFACE, 100000,
+                   temperature_ext_interr_handler)) {
+        printf("# %-70s%10s\n", "lm75a initialization", "...[OK]");
+    }
+    else {
+        printf("# %-70s%10s\n", "lm75a initialization", "...[FAILED]");
+    }
 
-	return EXIT_SUCCESS;
+    puts("LPC2387 is ready\n");
+    printf("PI=%f\n", 3.134343);
+    lm75A_start_sensor_sampling(temperature_ext_interrupt_task);
+
+    return EXIT_SUCCESS;
 }
 
