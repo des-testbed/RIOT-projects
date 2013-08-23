6e70493a3a7c6dd12c7ec60abbf80edceea44d37
Zakaria Kasmi <zkasmi@inf.fu-berlin.de>
2013-08-23 18:48:56 +0200

    Coding conventions.

--- test_shell/main.c
+++ test_shell/main.c
@@ -2,23 +2,24 @@
  * main.c - Main function of the test_shell project.
  * Copyright (C) 2013 Kaspar Schleiser <kaspar@schleiser.de>
  *
- * This source code is licensed under the LGPLv2 license, 
+ * This source code is licensed under the LGPLv2 license,
  * See the file LICENSE for more details.
- * 
+ *
  */
 
 /**
  * @file
  * @internal
- * @brief       shows how to set up own and use the system shell commands
- * 	        By typing help in the serial console, all the supported commands are listed.
+ * @brief       shows how to set up own and use the system shell commands.
+ *              By typing help in the serial console, all the supported commands
+ *              are listed.
  *
- * @author      Freie Universit√§t Berlin, Computer Systems & Telematics
+ * @author      Freie Universit‰t Berlin, Computer Systems & Telematics
  * @author      Kaspar Schleiser <kaspar@schleiser.de>
  * @author      Zakaria Kasmi <zkasmi@inf.fu-berlin.de>
- * @version     $Revision: 3854 $
+ * @version     $Revision: 3855 $
  *
- * @note        $Id: main.c 3854 2013-05-27 16:13:11 zkasmi $
+ * @note        $Id: main.c 3855 2013-09-04 17:00:33 kasmi $
  */
 
 #include <stdio.h>
@@ -29,53 +30,61 @@
 #include <shell.h>
 #include <board_uart0.h>
 
-void print_teststart(char* str) {
-	printf("[TEST_START]\n");
+void print_teststart(char *str)
+{
+    printf("[TEST_START]\n");
 
 }
 
-void print_testend(char* str) {
-	printf("[TEST_END]\n");
+void print_testend(char *str)
+{
+    printf("[TEST_END]\n");
 }
 
-void print_own_test(char * str) {
-	printf("[OWN_TEST]\n");
-	printf("[OWN_TEST]: input = %s\n", str);
+void print_own_test(char *str)
+{
+    printf("[OWN_TEST]\n");
+    printf("[OWN_TEST]: input = %s\n", str);
 }
 
-int shell_readc(void) {
-	char c = 0;
-	posix_read(uart0_handler_pid, &c, 1);
-	return c;
+int shell_readc(void)
+{
+    char c = 0;
+    posix_read(uart0_handler_pid, &c, 1);
+    return c;
 }
 
-void shell_putchar(int c) {
-	putchar(c);
+void shell_putchar(int c)
+{
+    putchar(c);
 }
 
-const shell_command_t shell_commands[] = { { "start_test", "starts a test",
-		print_teststart }, { "end_test", "ends a test", print_testend }, {
-		"start_own_test", "starts own test", print_own_test }, { NULL, NULL,
-		NULL } };
+const shell_command_t shell_commands[] = {
+    { "start_test", "starts a test", print_teststart },
+    { "end_test", "ends a test", print_testend },
+    { "start_own_test", "starts own test", print_own_test },
+    { NULL, NULL, NULL }
+};
 
-int main(void) {
-	//printf("Moin. build on %s %s SVN-Revision: %s\n", kernel_builddate, kernel_buildtime, kernel_svnrevision);
-	printf("test_shell.\n");
+int main(void)
+{
 
-	board_uart0_init();
+    printf("test_shell.\n");
 
-	posix_open(uart0_handler_pid, 0);
+    board_uart0_init();
 
-	//define own shell 
-	shell_t shell;
-	shell_init(&shell, shell_commands, shell_readc, shell_putchar);
-	shell_run(&shell);
+    posix_open(uart0_handler_pid, 0);
 
-	//init system shell
-	//shell_t sys_shell;
-	//shell_init(&sys_shell, NULL, shell_readc, shell_putchar);
-	//shell_run(&sys_shell);
+    //define own shell
+    shell_t shell;
+    shell_init(&shell, shell_commands, shell_readc, shell_putchar);
+    shell_run(&shell);
 
-	return 0;
+    //init system shell
+    //shell_t sys_shell;
+    //shell_init(&sys_shell, NULL, shell_readc, shell_putchar);
+    //shell_run(&sys_shell);
+
+    return 0;
 }
 
