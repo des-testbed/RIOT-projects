20a709035b9ee2b085ceaf84aef36f48cbe15834
Zakaria Kasmi <zkasmi@inf.fu-berlin.de>
2013-09-03 19:24:14 +0200

    SRF08 project, new handling of failed sensor initialization.

--- srf08-ultrasonic-sensor-i2c/main.c
+++ srf08-ultrasonic-sensor-i2c/main.c
@@ -10,17 +10,17 @@
 /**
  * @file
  * @internal
- * @brief               Initialize the SRF08 ultrasonic sensor and start a 
+ * @brief               Initialize the SRF08 ultrasonic sensor and start a
  *                      continuous sampling of the distance measures.
  *                      Up to 17 distances indicating echo's from distant
- *                      objects can be measured. The SRF08 ultrasonic sensor 
+ *                      objects can be measured. The SRF08 ultrasonic sensor
  *                      has a range of 3 cm to 6 m.
  *
  * @author              Freie Universität Berlin, Computer Systems & Telematics
  * @author              Zakaria Kasmi <zkasmi@inf.fu-berlin.de>
  * @version             $Revision: 3855 $
  *
- * @note                $Id: main.c 3855 2013-09-03 18:59:41 kasmi $
+ * @note                $Id: main.c 3855 2013-09-04 15:39:11 kasmi $
  */
 
 #include <stdio.h>
@@ -42,15 +42,18 @@ int main(void)
 
     // init the SRF08 ultrasonic sensor
     if (srf08_init(SRF08_I2C_INTERFACE, 400000)) {
-        printf("# %-70s%10s\n", "SRF08 ultrasonic sensor initialization", 
+        printf("# %-70s%10s\n", "SRF08 ultrasonic sensor initialization",
                "...[OK]");
+    }
+    else {
+        printf("# %-70s%10s\n", "SRF08 initialization", "...[FAILED]");
+        return EXIT_FAILURE;
     }
 
     // switch on the green LED
     LED_GREEN_ON;
     puts("MCU is ready\n");
     srf08_start_ranging(SRF08_REAL_RANGING_MODE_CM);
-
     return EXIT_SUCCESS;
 }
 
