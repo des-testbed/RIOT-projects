de8308d3ab57b3b616328d99d0a352a4ecc26bc1
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2014-01-04 13:29:17 +0100

    make test_hwtimer more flexible
    use all hwtimers available on the target platform

--- test_hwtimer/main.c
+++ test_hwtimer/main.c
@@ -16,9 +16,20 @@ int main(void)
 
     puts("Initializing hwtimer [OK].");
 
-    hwtimer_set(20000LU, callback, (void*) "callback1");
-    hwtimer_set(50000LU, callback, (void*) "callback2");
-    hwtimer_set(30000LU, callback, (void*) "callback3");
+#define BASE_DELAY 100000UL
+#define DELTA_DELAY 10000UL
+#define MSGLEN 12 // == strlen("callback %2i")
+    char msg[MSGLEN * ARCH_MAXTIMERS]; // == [callback  1\0callback  2\0...]
+    unsigned long delay = BASE_DELAY + (ARCH_MAXTIMERS * DELTA_DELAY);
+    /* set up to ARCH_MAXTIMERS-1 because hwtimer_wait below also
+     * needs a timer */
+    for (int i = 0; i < (ARCH_MAXTIMERS - 1); i++) {
+        char *msgn = msg + (i*MSGLEN);
+        delay -= DELTA_DELAY;
+        snprintf(msgn, MSGLEN, "callback %2x", i+1);
+        hwtimer_set(delay, callback, (void*) msgn);
+        printf("set %s\n", msgn);
+    }
 
     hwtimer_wait(HWTIMER_TICKS(1000UL * 1000UL));
 
